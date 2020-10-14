224feb0f7d47d53ebab4e432632d5ce981031cde
Szymon <jezyk2796@op.pl>
2020-10-14 19:52:20 +0200

    adding new tasks created

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 import Header from './components/header';
-import TodoItem from "./components/todoItem";
+import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo'
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -17,11 +18,23 @@ export default function App() {
     });
   };
 
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [
+        {
+          text: text,
+          key: Math.random().toString()
+        },
+        ...prevTodos
+      ]
+    })
+  };
+
   return (
     <View style={styles.container}>
       <Header />
       <View style={styles.content}>
-        {/* todo form */}
+        <AddTodo submitHandler={submitHandler} />
         <View style={styles.list}>
           <FlatList
             data={todos}
